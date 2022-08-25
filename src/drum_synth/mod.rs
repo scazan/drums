12ecfc49dc9b006bab4f2ce83eb53252417f7bf6
scazan <scottcazan@gmail.com>
2022-08-25 20:47:28 +0200

    cleanup

--- src/drum_synth/mod.rs
+++ src/drum_synth/mod.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::precedence)]
 
-use std::error::Error;
+// use std::error::Error;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use fundsp::hacker::*;
 use rand::Rng;
@@ -11,11 +11,11 @@ trait DrumSample {
 
 enum InstrumentType {
     Kick,
-    // Snare,
-    // Hihat,
-    // Rimshot,
-    // Clap,
-    // Tambourine,
+    Snare,
+    Hihat,
+    Rimshot,
+    Clap,
+    Tambourine,
 }
 
 struct KickSample {}
@@ -26,6 +26,7 @@ impl DrumSample for KickSample {
 
         let freq = rng.gen_range(60..=300) as f64;
         let overdrive = rng.gen_range(1..=7) as f64;
+
         let drum_sample =
             envelope(move |t| freq * exp(-t * 20.0)) >> sine() * overdrive >> shape(Shape::Tanh(2.0)) >> pan(0.0)
             >> (declick() | declick()) >> (dcblock() | dcblock())
@@ -58,71 +59,17 @@ where
     let sample_rate = config.sample_rate.0 as f64;
     let channels = config.channels as usize;
 
-    // let c = mls();
-    //let c = mls() >> lowpole_hz(400.0) >> lowpole_hz(400.0);
-    //let c = (mls() | dc(500.0)) >> butterpass();
-    //let c = (mls() | dc(400.0) | dc(50.0)) >> resonator();
-    // let c = pink();
-
-    // let m = 5.0;
-    // let c = oversample(sine_hz(freq) * freq * m + freq >> sine());
-
-    let drum_sample = KickSample::generate();
-    // Pulse wave.
-    // let c = lfo(|t| {
-        // let pitch = 110.0;
-        // let duty = lerp11(0.01, 0.99, sin_hz(0.05, t));
-        // (pitch, duty)
-    // }) >> pulse();
-
-    // let c = zero() >> pluck(70.0, 0.8, 0.8);
-    //let c = dc(110.0) >> dsf_saw_r(0.99);
-    // let c = dc(100.0) >> triangle();
-    //let c = lfo(|t| xerp11(20.0, 2000.0, sin_hz(0.1, t))) >> dsf_square_r(0.99) >> lowpole_hz(1000.0);
-    // let c = dc(110.0) >> square();
-
-    // Test ease_noise.
-    //let c = lfo(|t| xerp11(50.0, 5000.0, ease_noise(smooth9, 0, t))) >> triangle();
-
-    //let c = c
-    //    >> (pass() | envelope(|t| xerp(500.0, 20000.0, sin_hz(0.0666, t))))
-    //    >> bandpass_q(1.0);
-
-    // Waveshapers.
-    // let c = c >> shape_fn(|x| tanh(x * 5.0));
-
-    //let c = c & c >> feedback(butterpass_hz(1000.0) >> delay(1.0) * 0.5);
-
-    // Apply Moog filter.
-    // let c = (c | lfo(|t| (xerp11(110.0, 11000.0, sin_hz(9.15, t)), 0.6))) >> moog();
-
-    //let c = pink();
-
-    // let c = fundsp::sound::pebbles();
-
-    // let c = c * env() >> split::<U2>();
-    // let c = fundsp::sound::risset_glissando(false);
-
-    // Add chorus.
-    // let c = c >> (chorus(0, 0.015, 0.005, 0.5) | chorus(1, 0.015, 0.005, 0.5));
-
-    // Add flanger.
-    // let c = c
-        // >> (flanger(0.6, 0.005, 0.01, |t| lerp11(0.005, 0.01, sin_hz(0.1, t)))
-            // | flanger(0.6, 0.005, 0.01, |t| lerp11(0.005, 0.01, cos_hz(0.1, t))));
-
-    // Add phaser.
-    //let c = c >> (phaser(0.5, |t| sin_hz(0.1, t) * 0.5 + 0.5) | phaser(0.5, |t| cos_hz(0.1, t) * 0.5 + 0.5));
+    // figure out which kind of sample we are generating
+    let drum_sample = match instrument {
+        InstrumentType::Kick => KickSample::generate(),
+        _ => KickSample::generate(),
+    };
 
     let mut c = drum_sample;
-        // >> (0.8 * multipass() & 0.2 * reverb_stereo(10.0, 5.0))
-    // >> reverb_stereo(10.0, 5.0);
-    //let mut c = c * 0.1;
+
     c.reset(Some(sample_rate));
 
     let mut next_value = move || c.get_stereo();
-
-    // ^----  here is where it ends
 
     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
 
