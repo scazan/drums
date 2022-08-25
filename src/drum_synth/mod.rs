47576dcaa8fe59e6e37c741625a961243c4fc1f2
scazan <scottcazan@gmail.com>
2022-08-25 20:43:06 +0200

    extract to trait

--- src/drum_synth/mod.rs
+++ src/drum_synth/mod.rs
@@ -1,14 +1,12 @@
-//! Make some noise via cpal.
 #![allow(clippy::precedence)]
 
+use std::error::Error;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use fundsp::hacker::*;
-// use fundsp::math::{rnd};
 use rand::Rng;
 
-struct Params {
-    frequency: f64,
-    overdrive: f64,
+trait DrumSample {
+    fn generate() -> Box<dyn AudioUnit64>;
 }
 
 enum InstrumentType {
@@ -20,6 +18,23 @@ enum InstrumentType {
     // Tambourine,
 }
 
+struct KickSample {}
+
+impl DrumSample for KickSample {
+    fn generate() -> Box<dyn AudioUnit64> {
+        let mut rng = rand::thread_rng();
+
+        let freq = rng.gen_range(60..=300) as f64;
+        let overdrive = rng.gen_range(1..=7) as f64;
+        let drum_sample =
+            envelope(move |t| freq * exp(-t * 20.0)) >> sine() * overdrive >> shape(Shape::Tanh(2.0)) >> pan(0.0)
+            >> (declick() | declick()) >> (dcblock() | dcblock())
+            >> limiter_stereo((1.0, 5.0));
+
+        Box::new(drum_sample)
+    }
+}
+
 pub fn generate() {
     let host = cpal::default_host();
 
@@ -29,50 +44,30 @@ pub fn generate() {
 
     let config = device.default_output_config().unwrap();
 
-    let params = get_params(InstrumentType::Kick);
-
     match config.sample_format() {
-        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into(), params).unwrap(),
-        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into(), params).unwrap(),
-        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into(), params).unwrap(),
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into(), InstrumentType::Kick).unwrap(),
+        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into(), InstrumentType::Kick).unwrap(),
+        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into(), InstrumentType::Kick).unwrap(),
     }
 }
 
-fn get_params(instrument: InstrumentType) -> Params {
-    let mut rng = rand::thread_rng();
-
-    let rand_freq = rng.gen_range(60..=300) as f64;
-    let rand_overdrive = rng.gen_range(1..=7) as f64;
-
-    match instrument {
-        InstrumentType::Kick => Params {
-            frequency: rand_freq,
-            overdrive: rand_overdrive,
-        },
-    }
-}
-
-fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, params: Params) -> Result<(), anyhow::Error>
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, instrument: InstrumentType) -> Result<(), anyhow::Error>
 where
     T: cpal::Sample,
 {
     let sample_rate = config.sample_rate.0 as f64;
     let channels = config.channels as usize;
 
-    //let c = mls();
+    // let c = mls();
     //let c = mls() >> lowpole_hz(400.0) >> lowpole_hz(400.0);
     //let c = (mls() | dc(500.0)) >> butterpass();
     //let c = (mls() | dc(400.0) | dc(50.0)) >> resonator();
     // let c = pink();
 
-    let freq = params.frequency;
-    let overdrive = params.overdrive;
-    println!("FREQ: {}", freq);
     // let m = 5.0;
     // let c = oversample(sine_hz(freq) * freq * m + freq >> sine());
 
-    let bassdrum =
-        envelope(move |t| freq * exp(-t * 8.0)) >> sine() * overdrive >> shape(Shape::Tanh(2.0)) >> pan(0.0);
+    let drum_sample = KickSample::generate();
     // Pulse wave.
     // let c = lfo(|t| {
         // let pitch = 110.0;
@@ -105,7 +100,6 @@ where
 
     // let c = fundsp::sound::pebbles();
 
-    let env = || envelope(|t| exp(-t * 100.0));
     // let c = c * env() >> split::<U2>();
     // let c = fundsp::sound::risset_glissando(false);
 
@@ -120,10 +114,8 @@ where
     // Add phaser.
     //let c = c >> (phaser(0.5, |t| sin_hz(0.1, t) * 0.5 + 0.5) | phaser(0.5, |t| cos_hz(0.1, t) * 0.5 + 0.5));
 
-    let mut c = bassdrum
-        >> (declick() | declick()) >> (dcblock() | dcblock())
+    let mut c = drum_sample;
         // >> (0.8 * multipass() & 0.2 * reverb_stereo(10.0, 5.0))
-        >> limiter_stereo((1.0, 5.0));
     // >> reverb_stereo(10.0, 5.0);
     //let mut c = c * 0.1;
     c.reset(Some(sample_rate));
